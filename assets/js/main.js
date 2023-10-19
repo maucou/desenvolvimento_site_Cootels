e926ebfcfc14f5093c1ffb021107ddb466d42218
maucou <maucou123@hotmail.com>
2023-10-19 09:39:28 -0300

    creating the section contact us

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@ const buttonLearnMore1 = document.getElementById('btn-01')
 const buttonLearnMore2 = document.getElementById('btn-02')
 const buttonLearnMore3 = document.getElementById('btn-03')
 
+const buttonContactUs1 = document.getElementById('contact-btn-01')
+const buttonContactUs2 = document.getElementById('contact-btn-02')
+const buttonContactUs3 = document.getElementById('contact-btn-03')
+
+// button Learn More
 buttonLearnMore1.addEventListener('click', () => {
     let textSpan = document.getElementById('span1')
     textSpan.classList.toggle('expandText')
@@ -16,3 +21,4 @@ buttonLearnMore3.addEventListener('click', () => {
     let textSpan = document.getElementById('span3')
     textSpan.classList.toggle('expandText')
 })
+
